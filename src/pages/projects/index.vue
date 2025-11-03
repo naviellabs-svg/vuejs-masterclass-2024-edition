ed5b47cc9c2773a4a817fd607a98ec557a1c131d
Chris Conradie <naviel.labs@gmail.com>
2025-11-03 22:16:35 +0200

    Use Immediately Invoked Function Expression in Script Setup

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,13 +1,5 @@
 <script setup lang="ts">
-import { supabase } from '@/lib/supabaseClient'
 
-const getProjects = async () => {
-  const { data, error } = await supabase.from('projects').select()
-
-  if (error) console.log(error)
-
-  console.log('projects: ', data)
-}
 </script>
 <template>
   <div>
