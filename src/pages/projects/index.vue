c3d29d9f12935b48ac133be24ea84d73f76e4377
Chris Conradie <naviel.labs@gmail.com>
2025-11-03 21:55:53 +0200

    Query Supabase from Vue.js Script Setup

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,4 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+
+const getProjects = async () => {
+  const { data, error } = await supabase.from('projects').select()
+
+  if (error) console.log(error)
+
+  console.log('projects: ', data)
+}
+</script>
 <template>
   <div>
     <h1>Projects Page</h1>
