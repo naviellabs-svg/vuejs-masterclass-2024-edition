26cf5429b8853680b4c3d62af6c8a9e1e50c5396
Chris Conradie <naviel.labs@gmail.com>
2025-11-05 10:23:02 +0200

    Getting Started with Shadcn UI and Vue.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
+import path from 'node:path'
 import VueRouter from 'unplugin-vue-router/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [VueRouter(), vue(), vueDevTools()],
+  plugins: [VueRouter(), vue(), vueDevTools(), tailwindcss(), tsconfigPaths()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, './src')
     }
   }
 })
