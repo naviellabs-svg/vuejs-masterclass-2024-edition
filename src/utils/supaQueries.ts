052a8dee9c074094d67e6b85a6ec08157b9121c8
Chris Conradie <naviel.labs@gmail.com>
2025-11-12 15:03:27 +0200

    Exercise: Create Dynamic Indvidual Task Page

--- src/utils/supaQueries.ts
+++ src/utils/supaQueries.ts
@@ -15,7 +15,11 @@ export type TasksWithProjects = QueryData<typeof tasksWithProjectsQuery>
 export const projectsQuery = supabase.from('projects').select()
 export type Projects = QueryData<typeof projectsQuery>
 
-export const projectQuery = (slug: string) => supabase.from('projects').select(`
+export const projectQuery = (slug: string) =>
+  supabase
+    .from('projects')
+    .select(
+      `
  *,
  tasks(
  id,
@@ -23,8 +27,23 @@ export const projectQuery = (slug: string) => supabase.from('projects').select(`
  status,
  due_date
 )
-`)
-.eq('slug',slug)
-.single()
+`
+    )
+    .eq('slug', slug)
+    .single()
 
 export type Project = QueryData<ReturnType<typeof projectQuery>>
+
+export const taskQuery = (id: string) => {
+  return supabase.from('tasks').select(`
+    *,
+    projects (
+    id,
+    name,
+    slug
+    )
+    `)
+    .eq('id', id)
+    .single()
+}
+export type Task = QueryData<ReturnType<typeof taskQuery>>
