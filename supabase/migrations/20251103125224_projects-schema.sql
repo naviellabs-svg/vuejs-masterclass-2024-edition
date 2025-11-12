eb4cecfb8449573a4bdd7dfae84f40c01e219d02
Chris Conradie <naviel.labs@gmail.com>
2025-11-12 09:49:42 +0200

    Make the Project Page Template Dynamic

--- supabase/migrations/20251103125224_projects-schema.sql
+++ supabase/migrations/20251103125224_projects-schema.sql
@@ -8,6 +8,7 @@ CREATE TABLE
     created_at timestamp default now() not null,
     name text not null,
     slug text unique not null,
+    description text,
     status current_status  default 'in-progress' not null,
     collaborators text array default array[]::varchar[] not null
   );
