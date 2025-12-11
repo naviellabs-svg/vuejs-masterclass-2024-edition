8106a2c8b33a6013e2d1aba6aa954f1f0d6c0db1
Chris Conradie <naviel.labs@gmail.com>
2025-12-11 16:41:29 +0200

    Implement Realtime Form Validation

--- src/composables/formErrors.ts
+++ src/composables/formErrors.ts
@@ -1,16 +1,34 @@
-import type { AuthError } from "@supabase/supabase-js"
+import type { LoginForm } from '@/types/AuthForm'
+import type { AuthError } from '@supabase/supabase-js'
 
 export const useFormerrors = () => {
   const serverError = ref('')
-
+  const realtimeErrors = ref()
 
   const handelServerError = (error: AuthError) => {
     serverError.value =
-    error.message === 'Invalid login credentials' ? 'Incorrect email or password' : error.message
+      error.message === 'Invalid login credentials' ? 'Incorrect email or password' : error.message
+  }
+
+  const handelLoginForm = async (formData: LoginForm) => {
+    realtimeErrors.value = {
+      email: [],
+      password: []
+    }
+
+    const { validateEmail, validatePassword } = await import('@/utils/formValidations')
+
+    const emailErrors = validateEmail(formData.email)
+    if(emailErrors.length) realtimeErrors.value.email = emailErrors
+
+    const passwordErrors = validatePassword(formData.password)
+    if(passwordErrors.length) realtimeErrors.value.password = passwordErrors
   }
 
   return {
     serverError,
     handelServerError,
+    realtimeErrors,
+    handelLoginForm
   }
 }
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -6,7 +6,7 @@ const formData = ref({
   password: ''
 })
 
-const { serverError, handelServerError } = useFormerrors()
+const { serverError, handelServerError, realtimeErrors, handelLoginForm } = useFormerrors()
 
 const router = useRouter()
 
@@ -41,7 +41,13 @@ const signin = async () => {
               required
               v-model="formData.email"
               :class="{ 'border-red-500': serverError }"
+              @input="handelLoginForm(formData)"
             />
+            <ul class="text-sm text-left text-red-500" v-if="realtimeErrors?.password.length">
+              <li v-for="error in realtimeErrors.email" :key="error" class="list-disc">
+                {{ error }}
+              </li>
+            </ul>
           </div>
           <div class="grid gap-2">
             <div class="flex items-center">
@@ -56,6 +62,11 @@ const signin = async () => {
               v-model="formData.password"
               :class="{ 'border-red-500': serverError }"
             />
+            <ul class="text-sm text-left text-red-500" v-if="realtimeErrors?.password.length">
+              <li v-for="error in realtimeErrors.password" :key="error" class="list-disc">
+                {{ error }}
+              </li>
+            </ul>
           </div>
           <ul class="text-sm text-left text-red-500" v-if="serverError">
             <li class="list-disc">{{ serverError }}</li>
