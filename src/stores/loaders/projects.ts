c2a47b20685cc2147619f229944b78d51e7dd4a3
Chris Conradie <naviel.labs@gmail.com>
2025-12-15 14:08:32 +0200

    Update Stale Data with Fresh Data

--- src/stores/loaders/projects.ts
+++ src/stores/loaders/projects.ts
@@ -2,33 +2,29 @@ import { projectsQuery } from '@/utils/supaQueries'
 import { useMemoize } from '@vueuse/core'
 import type { Projects } from '@/utils/supaQueries'
 
-
-
 export const useProjectsStore = defineStore('projects-store', () => {
-  const projects = ref<Projects | null>(null)
+  const projects = ref<Projects>([])
   const loadProjects = useMemoize(async (key: string) => await projectsQuery)
 
   const validateCache = () => {
-if (projects.value?.length) {
-      projectsQuery.then(({ data }) => {
+    if (projects.value?.length) {
+      projectsQuery.then(({ data, error }) => {
         if (JSON.stringify(projects.value) === JSON.stringify(data)) {
-          console.log('Cached and fresh data matched!')
           return
         } else {
-          console.log('Something has changed!')
           loadProjects.delete('projects')
+          if (!error && data) projects.value = data
         }
       })
     }
   }
 
   const getProjects = async () => {
-
     const { data, error, status } = await loadProjects('projects')
 
     if (error) useErrorStore().setError({ error, customCode: status })
 
-    projects.value = data
+    if (data) projects.value = data
 
     validateCache()
   }
